020cf6a18e25aac7354e2b4e110d9071021f8cf2
Bohdan Dmytriv <bdmbdsm@users.noreply.github.com>
2018-05-18 17:21:40 +0300

    A674795074338498 add data structures (#5)
    * Add Ceasefire contract model
    
    * Add Milestone model
    
    * Fulfill Contract model with fields
    
    * Fix problems
    
    - Remove redefinition of built-in names
    - Remove redundant parameter

---
+++ openprocurement/contracting/ceasefire/constants.py
@@ -0,0 +1,23 @@
+# Model constants
+## Contract
+CONTRACT_STATUSES = (
+    'active.confirmation',
+    'active.payment',
+    'active.approval',
+    'active',
+    'terminated',
+    'unsuccesful',
+)
+## Milestone
+MILESTONE_TYPES = (
+    'financing',
+    'approval',
+    'reporting',
+)
+MILESTONE_STATUSES = (
+    'sheduled',
+    'processing',
+    'met',
+    'partiallyMet',
+    'notMet',
+)
