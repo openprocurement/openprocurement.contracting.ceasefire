b978e03b349330a2ee3a4a9294bf988c454247fc
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-06-12 16:39:12 +0300

    Add aliases logic

--- openprocurement/contracting/ceasefire/tests/includeme_test.py
+++ openprocurement/contracting/ceasefire/tests/includeme_test.py
@@ -14,16 +14,14 @@ class IncludemeTest(unittest.TestCase):
     def test_includeme(self, mocked_get_distribution, mocked_get_logger):
         config = Mock()
 
+        plugin_config = Mock()
+        plugin_config.get.side_effect = [[], True]
         logger = Mock()
         info = Mock()
         logger.info = info
         mocked_get_logger.return_value = logger
 
-        includeme(config)
+        includeme(config, plugin_config)
 
         assert info.called
         assert info.called_with('Init contracting.ceasefire plugin.')
-        assert str(config.mock_calls[0]) == \
-            "call.add_contract_contractType(<class 'openprocurement.contracting.ceasefire.models.Contract'>)"
-        assert str(config.mock_calls[1]) == \
-            "call.scan('openprocurement.contracting.ceasefire.views')"
