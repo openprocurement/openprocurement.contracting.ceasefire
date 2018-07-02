c05b5c0ce8c077f18c50001991c89ab867f7c0f2
Bohdan Dmytriv <bdmbdsm@users.noreply.github.com>
2018-07-02 17:17:39 +0300

    Add item fixture (#24)
    * Add items to the fixtures
    
    * Override items on ceasefire model
    
    * Update procuringEntity type + fixture

--- openprocurement/contracting/ceasefire/tests/fixtures/data.py
+++ openprocurement/contracting/ceasefire/tests/fixtures/data.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime, timedelta
+from copy import deepcopy
 
 from openprocurement.auctions.core.tests.base import (
     test_organization,
     test_procuringEntity,
 )
 
+
+swiftsure_procuring_entity = deepcopy(test_procuringEntity)
+swiftsure_procuring_entity.update({
+    "additionalContactPoints": [
+        {
+            "name": u"Державне управління справами",
+            "telephone": u"0440000000"
+        }
+    ]
+})
 
 contract_create_data = {
     'awardID': '376d560b2b2d452a80543865f3cab43e',
@@ -18,7 +29,37 @@ contract_create_data = {
     },
     'auction_token': '21c5ba429b1a434ea1bfa5f70a0f5885',
     'auction_id': '0b7bca6feeb644e987ded0429f1ec167',
-    'procuringEntity': test_procuringEntity,
+    'procuringEntity': swiftsure_procuring_entity,
     'title': 'Test Contract',
     'suppliers': [test_organization],
+    "items": [
+        {
+            "description": u"Земля для військовослужбовців",
+            "classification": {
+                "scheme": u"CPV",
+                "id": u"66113000-5",
+                "description": u"Земельні ділянки"
+            },
+            "additionalClassifications": [{
+                "scheme": "CAV",
+                "id": "39513200-3",
+                "description": "папір і картон гофровані, паперова й картонна тара"
+            }],
+            "unit": {
+                "name": u"item",
+                "code": u"44617100-9"
+            },
+            "quantity": 5,
+            "registrationDetails": {
+                "status": "unknown",
+            },
+            "address": {
+                "countryName": u"Україна",
+                "postalCode": "79000",
+                "region": u"м. Київ",
+                "locality": u"м. Київ",
+                "streetAddress": u"вул. Банкова 1"
+            }
+        }
+    ]
 }
