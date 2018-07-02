72fe53911b4fc318d9a22e41f51a749f09c0f691
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-07-02 10:13:09 +0300

    Add items to the fixtures

--- openprocurement/contracting/ceasefire/tests/fixtures/data.py
+++ openprocurement/contracting/ceasefire/tests/fixtures/data.py
@@ -21,4 +21,34 @@ contract_create_data = {
     'procuringEntity': test_procuringEntity,
     'title': 'Test Contract',
     'suppliers': [test_organization],
+    "items": [
+        {
+            "description": u"Земля для військовослужбовців",
+            "classification": {
+                "scheme": u"CPV",
+                "id": u"66113000-5",
+                "description": u"Земельні ділянки"
+            },
+            "additionalClassifications": [{
+                "scheme": "CAV",
+                "id": "39513200-3",
+                "description": "папір і картон гофровані, паперова й картонна тара"
+            }],
+            "unit": {
+                "name": u"item",
+                "code": u"44617100-9"
+            },
+            "quantity": 5,
+            "registrationDetails": {
+                "status": "unknown",
+            },
+            "address": {
+                "countryName": u"Україна",
+                "postalCode": "79000",
+                "region": u"м. Київ",
+                "locality": u"м. Київ",
+                "streetAddress": u"вул. Банкова 1"
+            }
+        }
+    ]
 }
