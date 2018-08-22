7e70d330e2c16a9fcbd1d851bd9e5f8dd973409d
Bohdan Dmytriv <dmytriv@quintagroup.org>
2018-08-22 12:39:39 +0300

    Remove wrong docstring

--- openprocurement/contracting/ceasefire/tests/fixtures/helpers.py
+++ openprocurement/contracting/ceasefire/tests/fixtures/helpers.py
@@ -188,8 +188,6 @@ def prepare_milestones_reporting(test_case, contract_data=None):
 
 
 def prepare_milestones_all_met(test_case, contract_data=None):
-    """Prepares contract's milestones to make approval milestone have processing status
-    """
     contract, milestones = prepare_milestones_reporting(test_case, contract_data)
     reporting_milestone = search_list_with_dicts(milestones, 'type_', 'reporting')
     dateMet_to_set = reporting_milestone.dueDate - timedelta(days=5)
