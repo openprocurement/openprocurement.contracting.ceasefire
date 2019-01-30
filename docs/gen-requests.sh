b2a6824d60fde6fd74414f9ede96951e5a6b2af4
oleksiyVeretiuk <32167775+oleksiyVeretiuk@users.noreply.github.com>
2019-01-30 17:07:48 +0200

    Revert "Polish documentation generation (#54)"
    This reverts commit 12260feae9d8c4b717fe3953b2c3f49247da1446.

--- docs/gen-requests.sh
+++ docs/gen-requests.sh
@@ -1,9 +1,6 @@
-# check if there is buildout present
-if [ ! -e bin/buildout ] || [ ! -e bin/nosetests ]
-# if it isn't - create it
+#!/bin/zsh
+if [ -e bin/docs ]
 then
-    python bootstrap.py
     ./bin/buildout
 fi
-# run the requests files generation
 ./bin/nosetests -sv docs/update_tutorial_requests.py
--- docs/makedocs.sh
+++ docs/makedocs.sh
@@ -1,10 +1,6 @@
-# bootstrap the buildout if needed
-if [ ! -e bin/buildout ]; then
-    python bootstrap.py
-fi
-
-if [ ! -e bin/docs ]; then
+#!/bin/zsh
+if ! [ -e bin/docs ]
+then
     ./bin/buildout -c docs.cfg
 fi
-
 ./bin/docs
