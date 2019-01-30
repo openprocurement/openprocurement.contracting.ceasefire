12260feae9d8c4b717fe3953b2c3f49247da1446
Bohdan Dmytriv <7142689+bdmbdsm@users.noreply.github.com>
2019-01-30 17:01:24 +0200

    Polish documentation generation (#54)
    * Use `PrefixedRequestClass` from the API package
    
    It was imported from the auctions.core, therefore it has broke the build.
    
    * Update gen-requests
    
    - Add comments
    - Do a better check for buildout presence
    
    * Do deeper checks in the `makedocs.sh`
    
    * Simplify the docs build manual
    
    * Check on the nosetest script presence
    
    * Fix new packages' versions

--- docs/makedocs.sh
+++ docs/makedocs.sh
@@ -1,6 +1,10 @@
-#!/bin/zsh
-if ! [ -e bin/docs ]
-then
+# bootstrap the buildout if needed
+if [ ! -e bin/buildout ]; then
+    python bootstrap.py
+fi
+
+if [ ! -e bin/docs ]; then
     ./bin/buildout -c docs.cfg
 fi
+
 ./bin/docs
