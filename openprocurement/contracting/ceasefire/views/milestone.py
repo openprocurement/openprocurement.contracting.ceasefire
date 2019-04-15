01adb4b51f601c173b9ae14ee8e90dbbe1d08a1b
Bohdan Dmytriv <dmytriv@quintagroup.org>
2019-04-15 15:44:11 +0300

    Milestone views refactor

--- openprocurement/contracting/ceasefire/views/milestone.py
+++ openprocurement/contracting/ceasefire/views/milestone.py
@@ -1,24 +1,17 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
-
 from openprocurement.api.utils import (
     APIResource,
-    context_unpack,
     json_view,
 )
+from openprocurement.api.utils.error_management import handle_errors_on_view
 from openprocurement.contracting.core.utils import (
-    apply_patch,
     contractingresource,
-)
-from openprocurement.contracting.core.interfaces import (
-    IMilestoneManager,
 )
 from openprocurement.contracting.ceasefire.constants import (
     ENDPOINTS,
 )
-from openprocurement.contracting.ceasefire.validators import (
-    validate_patch_milestone_data,
-)
+from openprocurement.api.utils.validation import validate_data_to_event
+from openprocurement.contracting.core.manager_discovery import ContractManagerDiscovery
 
 
 @contractingresource(
@@ -36,19 +29,11 @@ class CeasefireMilestoneResource(APIResource):
     @json_view(
         permission='edit_contract',
         content_type='application/json',
-        validators=(validate_patch_milestone_data,))
+        validators=(validate_data_to_event,))
+    @handle_errors_on_view
     def patch(self):
-        manager = self.request.registry.getAdapter(self.request.context, IMilestoneManager)
-        manager.change_milestone(self.request)
-        self.request.context.dateModified = datetime.now()
-        if apply_patch(self.request):
-            self.LOGGER.info(
-                'Updated ceasefire milestone {}'.format(
-                    self.request.context.id
-                ),
-                extra=context_unpack(
-                    self.request,
-                    {'MESSAGE_ID': 'ceasefire_milestone_patch'}
-                    )
-                )
-            return {'data': self.request.context.serialize()}
+        event = self.request.event
+        md = ContractManagerDiscovery(self.request.registry.manager_registry)
+        contract_manager = md.discover(event.ctx.high)
+        manager = contract_manager.milestone_manager()
+        return manager.change_milestone(event)
